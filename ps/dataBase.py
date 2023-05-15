d46c31f915b177361f44ccb7d109fd5b4f4f3cb3
Jack Merma <jmermap@unsa.edu.pe>
2023-05-15 00:36:30 -0500

    simple BD

---
+++ ps/dataBase.py
@@ -0,0 +1,14 @@
+cuentas = {
+    '001' : ['Dave', 5000],
+    '123' : ['Luna', 4000],
+    '456' : ['Joel', 6000],
+    '666' : ['Jack', 5500],
+    '341' : ['Danitza', 4010],
+}
+
+salida = {
+    'Monto no valido' : 0,
+    'Monto no valido para menos de 3000 soles' : 1,
+    'Monto no permitido de retirar > 3000 soles' : 2,
+    'Imposible de realizar, su monto es menor' : 3,
+}
